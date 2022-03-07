c3055e83b930370080bf730782d96d80e779c6a3
Hikaru Inomata <inomatahi@icloud.com>
2022-03-07 00:34:55 +0000

    section12 finish

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
-import { ProjectInput } from './components/project-input';
-import { ProjectList } from './components/project-list';
+import { Product } from "./product.model";
 
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
+const products = [
+  { title: "商品1", price: 100 },
+  { title: "商品2", price: 200 },
+];
+
+// const p1 = new Product("商品1", 100);
+
+const loadedProducts = products.map(prod => {
+  return new Product(prod.title, prod.price);
+})
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
